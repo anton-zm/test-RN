bea8b243ed6df4cf28e08febbc239eb0d264c42c
anton-zm <antonzm34@gmail.com>
2021-07-12 23:05:32 +0300

    test work

--- src/screens/intro-screen.tsx
+++ src/screens/intro-screen.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, Image, TouchableOpacity } from 'react-native';
 import Fade from 'react-native-fade'
+import  bg  from '../assets/img/bg.jpg'
 
 export const Intro = ({ navigation }) => {
     const [fadeIn, setFadeIn] = useState(false)
@@ -10,11 +11,12 @@ useEffect(() => {
     setTimeout(() => {
         setFadeIn(true)
     }, 2000)
-},[])
+})
 
   return (
     <View style={styles.container}>
-        <Fade duration={2000} visible={fadeIn} direction={'down'}>
+        <Image style={styles.bg} source={bg} />
+        <Fade duration={2000} visible={fadeIn} direction={'up'}>
             <Text style={styles.title}>My App</Text>
         </Fade>
       
@@ -33,10 +35,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#4c4ff1',
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative',
+    zIndex:2
   },
   title:{
     color:'white',
     fontSize: 58,
-    marginBottom: 48
+    marginBottom: 48,
+  },
+  bg:{
+      position: 'absolute',
+      opacity: 0.5,
+      width: '100%',
+      height: '100%',
+      left:0,
+      top:0,
+      zIndex: 0
   }
 });
